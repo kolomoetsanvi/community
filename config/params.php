ec929787ebfa3d6bc5a44f7968fef3b8929801a7
kolomoetsanvi <57458848+kolomoetsanvi@users.noreply.github.com>
2021-06-17 13:59:27 +0300

    Update params.php

--- config/params.php
+++ config/params.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'accessToken' => 'eafa3f9eb2d0513f55e94e474d740c23f16900322643d10892b03fb184582414ea082ff2966d2584c8b05',
-    'ApiID' => 7880069
+    'accessToken' => '',
+    'ApiID' => 
 ];
